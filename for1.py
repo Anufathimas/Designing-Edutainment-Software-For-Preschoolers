fe7bcdd6b0892f37b5f6b15a7f9d0261be625ba3
Anufathimas <71185004+Anufathimas@users.noreply.github.com>
2021-06-03 13:57:42 +0700

    Add files via upload

---
+++ for1.py
@@ -0,0 +1,22 @@
+def fun():
+	car=["bmw","zen","figo"]
+	for x in car:
+		print(x)
+fun()
+def fun1():
+	car=["bmw","zen","figo"]
+	for x in car:
+		print(x)
+		if(x=="zen"):
+			break
+fun1()
+def mytuple():
+	tuplecar=("bmw","zen","figo","benz","bmw")
+	print(tuplecar)
+	print(len(tuplecar))
+mytuple()
+def myset():
+	setcar={"bmw","zen","figo","benz","bmw"}
+	print(setcar)
+	print(len(setcar))
+myset()
